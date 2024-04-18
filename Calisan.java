2f65cb08b95c3b456ee8e6d5e78f4bf7dc9714b0
MLHIPK <167212344+MLHIPK@users.noreply.github.com>
2024-04-18 22:06:33 +0300

    Create Calisan.java

---
+++ Calisan.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Calisan {
+    private String ad;
+    private String soyad;
+
+    public Calisan(String ad, String soyad) {
+        this.ad = ad;
+        this.soyad = soyad;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+}
